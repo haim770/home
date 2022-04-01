ef45111ed23113d56991f2bcd14f58305bb3b6aa
Lidor ben shimol <lidorag2@gmail.com>
2022-04-01 11:35:11 +0300

    home SQL file

---
+++ homedb.sql
@@ -0,0 +1,384 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2022 at 10:22 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `homedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+
+CREATE TABLE `ads` (
+  `adID` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_id` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  `contact_counter` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `views` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `close_reason` text DEFAULT NULL,
+  `expire_date` datetime NOT NULL,
+  `approval_status` varchar(50) NOT NULL DEFAULT 'pending',
+  `ad_link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ad_content`
+--
+
+CREATE TABLE `ad_content` (
+  `element_id` varchar(50) NOT NULL,
+  `adId` varchar(50) NOT NULL COMMENT 'when master is active adid will be zero (0)',
+  `category` varchar(50) NOT NULL COMMENT 'exp, rent, buy...',
+  `master` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'defind if row is for new add properties',
+  `min_value` double DEFAULT NULL COMMENT 'min value will appear only for chooseing parameter to set minmum row value',
+  `max_value` double DEFAULT NULL COMMENT 'min value will appear only for chooseing parameter to set minmum row value',
+  `icon` varchar(255) NOT NULL COMMENT 'use to set the row thumbnail show in ad',
+  `free_text` text NOT NULL,
+  `required` tinyint(1) NOT NULL DEFAULT 0,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `blog_id` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(50) NOT NULL DEFAULT 'Draft' COMMENT 'active, close, draft',
+  `title` varchar(255) NOT NULL COMMENT 'blog title',
+  `userId` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `update_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `views` int(10) UNSIGNED NOT NULL,
+  `cover_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `favorite_id` varchar(50) NOT NULL,
+  `userId` varchar(50) NOT NULL,
+  `AdId` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `history_id` varchar(50) NOT NULL,
+  `userId` varchar(50) NOT NULL,
+  `adId` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
+CREATE TABLE `links` (
+  `linkId` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `userId` varchar(50) NOT NULL,
+  `link_content` text NOT NULL,
+  `link` varchar(255) NOT NULL COMMENT 'link url',
+  `views` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msgid` varchar(255) NOT NULL,
+  `adId` varchar(50) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `receiver` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `files` text NOT NULL,
+  `dateMsg` datetime NOT NULL,
+  `seen` int(4) NOT NULL DEFAULT 0,
+  `received` int(4) NOT NULL DEFAULT 0,
+  `delete_sender` tinyint(4) NOT NULL DEFAULT 0,
+  `delete_receiver` tinyint(4) NOT NULL DEFAULT 0,
+  `newUpdate` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `package`
+--
+
+CREATE TABLE `package` (
+  `packageId` varchar(50) NOT NULL,
+  `price` double NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `ad_value` int(10) UNSIGNED NOT NULL COMMENT 'number of ad''s added after purches',
+  `update_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `life_cycle` datetime NOT NULL COMMENT 'the time the package will be avalible'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_recovery`
+--
+
+CREATE TABLE `password_recovery` (
+  `id` int(11) NOT NULL,
+  `userMail` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `exp_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pictures`
+--
+
+CREATE TABLE `pictures` (
+  `pictureID` varchar(50) NOT NULL,
+  `element_id` varchar(50) NOT NULL,
+  `serial_number` int(11) UNSIGNED NOT NULL,
+  `picture_url` varchar(255) NOT NULL,
+  `upload_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `alt` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_history`
+--
+
+CREATE TABLE `purchase_history` (
+  `purchase_id` varchar(50) NOT NULL,
+  `packageId` varchar(50) NOT NULL,
+  `userId` varchar(50) NOT NULL,
+  `purchase_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_reason`
+--
+
+CREATE TABLE `report_reason` (
+  `reason_id` varchar(50) NOT NULL,
+  `element_type` varchar(255) NOT NULL COMMENT 'blog,ad...',
+  `reason_name` varchar(255) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_messages`
+--
+
+CREATE TABLE `system_messages` (
+  `msgId` varchar(50) NOT NULL,
+  `userId` varchar(50) NOT NULL,
+  `message_content` text NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `seen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uuid` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `password` varchar(50) NOT NULL,
+  `last_seen` datetime NOT NULL,
+  `prompt` varchar(255) NOT NULL,
+  `rule` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_reports`
+--
+
+CREATE TABLE `user_reports` (
+  `report_id` varchar(50) NOT NULL,
+  `element_id` varchar(50) NOT NULL,
+  `userId` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `content` text NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `manage_feedback` text NOT NULL,
+  `report_reason` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`adID`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `ad_content`
+--
+ALTER TABLE `ad_content`
+  ADD PRIMARY KEY (`element_id`),
+  ADD KEY `adId` (`adId`);
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`blog_id`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`favorite_id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `AdId` (`AdId`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`history_id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `adId` (`adId`);
+
+--
+-- Indexes for table `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`linkId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msgid`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`),
+  ADD KEY `adId` (`adId`);
+
+--
+-- Indexes for table `package`
+--
+ALTER TABLE `package`
+  ADD PRIMARY KEY (`packageId`);
+
+--
+-- Indexes for table `password_recovery`
+--
+ALTER TABLE `password_recovery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pictures`
+--
+ALTER TABLE `pictures`
+  ADD PRIMARY KEY (`pictureID`);
+
+--
+-- Indexes for table `purchase_history`
+--
+ALTER TABLE `purchase_history`
+  ADD PRIMARY KEY (`purchase_id`),
+  ADD KEY `packageId` (`packageId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `system_messages`
+--
+ALTER TABLE `system_messages`
+  ADD PRIMARY KEY (`msgId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uuid`);
+
+--
+-- Indexes for table `user_reports`
+--
+ALTER TABLE `user_reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `report_reason` (`report_reason`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `password_recovery`
+--
+ALTER TABLE `password_recovery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
