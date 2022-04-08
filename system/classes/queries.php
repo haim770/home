23af805c2e9385c279c9136a2b48d3ed0d44e253
lidorag <lidorag2@gmail.com>
2022-04-08 12:24:41 +0300

    change queries

--- system/classes/queries.php
+++ system/classes/queries.php
@@ -1,23 +1,21 @@
 <?php
 require_once('useDbs.php');
-require_once('test.php');
 $db = dbClass::GetInstance();
-//$query = "CALL `getFirstNameLastName`('lidor','ben shimol');";
+//$query = "CALL `getFirstNameLastName`('lidor','ben shimol');";+
 
-enum readQ: string{
-    case getAllAds = "CALL `getAdsTable`();";
-    case getAllUsers = "CALL `getUsersTable`();";
-}
+$readDB = array();
+$readDB = [
+    "GetAllUsers"=> "CALL `getUsersTable`();",
+    "GetAllAds"=> "CALL `getAdsTable`();"
+];
 
-enum writeDB: string
-{
-    TEST
+function _r($read){
+    global $readDB;
+    return $readDB[$read];
 }
+$query = _r("GetAllUsers");
 
-enum updateDB: string
-{
-}
-$query = readQ::getAllUsers;
+
 $data = $db->readDB($query, []);
 if(is_array($data)){
     foreach($data as $row){
