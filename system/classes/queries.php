7557e5b0fd166e5b55e7ad8ef23b3f6c5e26feeb
lidorag <lidorag2@gmail.com>
2022-04-08 11:05:41 +0300

    Try adding enum

--- system/classes/queries.php
+++ system/classes/queries.php
@@ -1,21 +1,22 @@
 <?php
 require_once('useDbs.php');
+require_once('test.php');
 $db = dbClass::GetInstance();
 //$query = "CALL `getFirstNameLastName`('lidor','ben shimol');";
 
-enum readDB: string{
+enum readQ: string{
     case getAllAds = "CALL `getAdsTable`();";
     case getAllUsers = "CALL `getUsersTable`();";
 }
 
-enum writeDB: string {
-
+enum writeDB: string
+{
 }
 
-enum updateDB: string {
-
+enum updateDB: string
+{
 }
-$query = readDB::getAllAds;
+$query = readQ::getAllUsers;
 $data = $db->readDB($query, []);
 if(is_array($data)){
     foreach($data as $row){
