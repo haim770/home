cb67a603107531b0297266ce4092882dbf352b60
lidorag <lidorag2@gmail.com>
2022-04-08 10:44:10 +0300

    Adding new enums with stats

--- system/classes/queries.php
+++ system/classes/queries.php
@@ -1,9 +1,22 @@
 <?php
 require_once('useDbs.php');
 $db = dbClass::GetInstance();
-$query = "CALL `getFirstNameLastName`('lidor','ben shimol');";
-$data = $db->readDB($query,[]);
+//$query = "CALL `getFirstNameLastName`('lidor','ben shimol');";
 
+enum readDB: string{
+    case getAllAds = "CALL `getAdsTable`();";
+    case getAllUsers = "CALL `getUsersTable`();";
+}
+
+enum writeDB: string {
+
+}
+
+enum updateDB: string {
+
+}
+$query = readDB::getAllAds;
+$data = $db->readDB($query, []);
 if(is_array($data)){
     foreach($data as $row){
         print_r($row);
