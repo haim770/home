59eeb89d10c02764fc4e838e15e04f86f0ef5284
haim770 <haim17701@gmail.com>
2022-04-30 21:40:04 +0300

    haim commit b4 saw lidors commit

--- home-app/src/Components/Nav.js
+++ home-app/src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Button from "./Button";
+import NavPart from "./NavPart";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
 import "./Nav.css";
 function Nav(props) {
@@ -7,13 +7,19 @@ function Nav(props) {
   return (
     <div>
       <nav className={navClass}>
-        <Link to="/">
-          <img src={require("../pics/logoHome.jpg")} alt="logo" />
-        </Link>
-        |{"  "}
-        <Link to="/listAds">list ads</Link>|{"  "}
-        <Link to="/testAxios">Test Axios</Link> |{"  "}
-        <Link to="/parameter">parameter</Link> |
+        <NavPart
+          part="logo" //name of the part of the navbar
+          image={<img src={require("../pics/logoHome.jpg")} alt="logo" />}
+        />
+        <NavPart
+          part="main"
+          listAdsLink="/listAds"
+          listAdsLinkContent="list ads"
+          parameterLink="/parameter"
+          parameterLinkContent="parameter"
+          TestAxiousLink="/testAxios"
+          TestAxiousLinkContent="test axious"
+        />
       </nav>
 
       <Outlet />
---
+++ home-app/src/Components/NavPart.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import "./Nav.css";
+function NavPart(props) {
+  const returnWantedPart = () => {
+    //return the wanted content for the component
+    let code = "";
+    if (props.part === "logo") {
+      code = (
+        <nav>
+          <Link to={props.homeLink}>{props.homeLinkContent}</Link>
+        </nav>
+      );
+    } else {
+      if (props.part === "main") {
+        code = (
+          <nav>
+            <Link to={props.listAdsLink}>{props.listAdsLinkContent}</Link>|
+            {"  "}
+            <Link to={props.TestAxiousLink}>{props.TestAxiousLinkContent}</Link>
+            |{"  "}
+            <Link to={props.parameterLink}>{props.parameterLinkContent}</Link>|
+            {"  "} ;
+          </nav>
+        );
+      } else {
+      }
+    }
+
+    return code;
+  };
+  return returnWantedPart();
+}
+NavPart.defaultProps = {
+  homeLink: "/",
+  homeLinkContent: <img src={require("../pics/logoHome.jpg")} alt="logo" />,
+  listAdsLink: "/listAds",
+  listAdsLinkContent: "list ads",
+  parameterLink: "/parameter",
+  parameterLinkContent: "parameter",
+  TestAxiousLink: "/testAxios",
+  TestAxiousLinkContent: "test axious",
+  part: "", //name of the part [logo,userPart,mainPart]
+};
+export default NavPart;
