f025c8696eba7dafbf3bd6665895b362d15cd2f6
lidorag <lidorag2@gmail.com>
2022-05-03 22:27:22 +0300

    Update Nav bar and styles

---
+++ home-app/src/Components/NavRoots.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navigation/navStyle.css";
+import "../styles/navigation/responsiveNavigation.css";
+const NavRoot = () => {
+  const menuData = [
+    {
+      path: "/",
+      name: "בית",
+    },
+    {
+      path: "/Ads",
+      name: "מודעות",
+    },
+    {
+      path: "/testAxios",
+      name: "בדיקת AXIOS",
+    },
+  ];
+  return (
+    <nav className="MuiPaper-root MuiAppBar-root jss4 MuiAppBar-positionSticky MuiAppBar-colorPrimary jss2 MuiPaper-elevation4">
+      {/* Inner Nav bar */}
+      <div className="MuiToolbar-root jss4 jss5 MuiToolbar-regular MuiToolbar-gutters">
+        {/* Site Logo */}
+        <label className="homeLogo jss6">
+          <NavLink to="/" key="Home">
+            <img
+              src={require("../pics/logoHome.png")}
+              alt="logo"
+              className="jss10"
+            />
+          </NavLink>
+        </label>
+
+        <div className="menu jss14 jss8">
+          <div className="jss13">
+            {
+              // This section build our navigation NavLinks, if we want to add new link we just need to add it to menuData element.
+              menuData.map((item) => (
+                <div className="jss12">
+                  <NavLink
+                    to={item.path}
+                    key={item.name}
+                    className="MuiButtonBase-root MuiButton-root jss22 MuiButton-text"
+                  >
+                    <div className="list_item ">{item.name}</div>
+                  </NavLink>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavRoot;
--- home-app/src/index.js
+++ home-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
+/*
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Expenses from "./expenses";
 import Invoices from "./invoices";
@@ -9,13 +10,16 @@ import Invoice from "./invoice";
 import Parameter from "./Components/Parameter";
 import TestAxios from "./testAxios";
 import Register from "./users/Register";
-import Nav from "./Components/Nav";
-import Api from "./api/Api";
-import ListAds from "./Components/ListAds";
-const api = new Api();
+import Nav from "./Components/Nav";*/
+//import Api from "./api/Api";
+
+//import ListAds from "./Components/ListAds";
+//const api = new Api();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App/>
+  {/*
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
@@ -28,6 +32,6 @@ root.render(
           <Route path="parameter" element={<Parameter paramName={"3"} />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>*/}
   </React.StrictMode>
 );
