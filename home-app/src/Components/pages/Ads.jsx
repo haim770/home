2ec4fa40ec8fe10b3f961c3e8ab796919b693cd4
lidorag <lidorag2@gmail.com>
2022-05-05 22:41:48 +0300

    Adding simple and working axios to Pages folder, also we need to convert all files to JSX files to make sure all our code will work currectly.

--- home-app/src/Components/pages/Ads.jsx
+++ home-app/src/Components/pages/Ads.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import TestAxios from "./TestAxios";
 
 import "../../styles/Main.css";
 const Ads = () => {
   return (
     <div>
       <p>Ads page</p>
+      <TestAxios data_type="TEST" params={""} />
     </div>
+    
   );
 };
 export default Ads;
