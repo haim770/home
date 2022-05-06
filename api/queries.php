5db2e64f1c3e5b8261b6239c27acded119bb074c
haim770 <haim17701@gmail.com>
2022-05-06 10:31:46 +0300

    RECENT COMMIT

--- api/queries.php
+++ api/queries.php
@@ -7,19 +7,3 @@ enum queries :string{
 $db = new useDbs();
 echo $db->readDBObj(q)
 ?>
-
-
-if($dataType=='add parameter ads'){
-    addParameterAds();
-}
-}
-function addParameterAds(){
-    global $db;
-    global $arr;
-    $arr['paramName']=$_POST['paramName'];
-    $arr['paramType']=$_POST['paramType'];
-    $query="ALTER TABLE ads ADD :paramName :paramType;"
-    $result=$db->writeDBNotStoredProcedure($query,$arr);
-    $arr=[];
-	echo json_encode ($result);
-}
