7d29e3234572b4855ec74b41d3c6ea91197c389f
haim770 <haim17701@gmail.com>
2022-05-07 20:23:19 +0300

    hai

---
+++ home-app/configParameters.js
@@ -0,0 +1,62 @@
+export const parameters = [
+  {
+    paramName: "air conditioner",
+    paramStyle: "checkBox",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "price",
+    paramStyle: "input",
+    paramMinValue: "0",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "ad link",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "city",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "street",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "apartment",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "entry",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "rooms",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "create time",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+  {
+    paramName: "number",
+    paramStyle: "input",
+    paramMinValue: "",
+    paramMaxValue: "",
+  },
+];
