eb64e6d747bf5378c2d811f0e43ac601dbc397de
lidorag <lidorag2@gmail.com>
2022-05-23 14:18:03 +0300

    ץ

--- home-app/src/App.js
+++ home-app/src/App.js
@@ -1,18 +1,9 @@
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 import Footer from "./Components/Footer.js";
 import NavRoots from "./Components/NavRoots.js";
 import Main from "./Components/Main.js";
 import AdsWithSearch from "./Components/AdsWithSearch.js";
-import PackageDisplay from "./Components/PackageDisplay.js";
-import CreatePackage from "./Components/CreatePackage.js";
-import { useHistory } from "react-router-dom";
-import Package1 from "./Components/Package1.js";
-import AddAdForm from "./Components/AddAdForm.js";
-import PackageFull from "./Components/PackageFull.js";
-import Ads from "./Components/pages/Ads.jsx";
-import AdFull from "./Components/AdFull.js";
-import AddParameterToAds from "./Components/AddParameterToAds.js";
 
 // NEW //
 import Unauthorized from "./Components/pages/Unauthorized";
@@ -34,25 +25,6 @@ export default function App() {
     <div className="App">
       <NavRoots />
       <Routes>
-        <Route exact path="/main" element={<Main />}>
-          <Route exact path=":linkAd" element={<AdFull />} />
-          <Route exact path="Ads" element={<Ads />} />
-          <Route exact path="AddAdForm" element={<AddAdForm />} />
-          <Route
-            exact
-            path="AddParameterToAds"
-            element={<AddParameterToAds />}
-          />
-          <Route exact path="Login" element={<LoginPage />} />
-          <Route exact path="adsWithSearch" element={<AdsWithSearch />} />
-          <Route
-            exact
-            path="/main/packages/:packageId"
-            element={<PackageFull />}
-          />
-          <Route exact path="packages" element={<PackageDisplay />} />
-          <Route exact path="addPack" element={<CreatePackage />} />
-        </Route>
         <Route path="/" element={<Layout />}>
           {/* public routes */}
           <Route path="/" element={<Main />} />
