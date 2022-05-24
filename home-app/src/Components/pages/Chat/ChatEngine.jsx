680f403a89a308383ec95a069c0efb3a686d2e60
lidorag <lidorag2@gmail.com>
2022-05-24 14:13:50 +0300

    Fix some Chat engin data

--- home-app/src/Components/pages/Chat/ChatEngine.jsx
+++ home-app/src/Components/pages/Chat/ChatEngine.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
 import { styles } from "./styles";
+import React from "react";
 
 import useAuth from "../../../Auth/useAuth";
 
 const ChatEngine = () => {
-      const { auth } = useAuth();
-  return (
-      // if we login show chat, else dont show
-      auth.accessToken?
-    <div
-      className="transition-5"
-      style={{
-        ...styles.chatWithMeButton,
-      }}
-    ></div>:<></>
-  );
-}
+  const { auth } = useAuth();
 
-export default ChatEngine
+  return (
+    // if we login show chat, else dont show
+    auth.accessToken ? (
+      <div
+        className="transition-5"
+        style={{
+          ...styles.chatWithMeButton,
+        }}
+      ></div>
+    ) : (
+      <></>
+    )
+  );
+};
+
+export default ChatEngine;
