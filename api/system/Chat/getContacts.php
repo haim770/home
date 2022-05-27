0beb6563d05eb6d607050f1f79c6aeda681799a9
lidorag <lidorag2@gmail.com>
2022-05-27 12:07:15 +0300

    Add ServerSide token verifiy

--- api/system/Chat/getContacts.php
+++ api/system/Chat/getContacts.php
@@ -4,5 +4,5 @@ include("../../Authentication/authTest.php");
 
 $arr = []; //for global scope var
 $query = "SELECT firstName, lastName, uuid from users where uuid in (SELECT DISTINCT sender from messages where receiver = :alice UNION SELECT DISTINCT receiver from messages where sender = :alice)";
-     
+$username = $token->data->username;
 ?>
