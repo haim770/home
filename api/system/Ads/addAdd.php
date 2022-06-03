06900fbcbc4dd6ec6f8fbc953c8414971fdd30cc
lidorag <lidorag2@gmail.com>
2022-06-03 10:20:46 +0300

    add auth to formAdContact

---
+++ api/system/Ads/addAdd.php
@@ -0,0 +1,12 @@
+<?php
+// get authTest file
+$authPath = "../../Authentication/authTest.php";
+include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . $authPath);
+
+$arr = []; //for global scope var
+$query = "SELECT uuid from users where mail = :alice";
+$arr["alice"] = $token->data->user;
+$arr["alice"] = ($db->readDBNoStoredProcedure($query, $arr))[0]->uuid;
+
+
+?>
