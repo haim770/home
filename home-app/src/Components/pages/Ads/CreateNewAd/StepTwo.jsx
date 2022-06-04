f5da735fc49434f0192b8aabd0872b3704fac277
lidorag <lidorag2@gmail.com>
2022-06-04 16:00:48 +0300

    Remove react select, wont working

--- home-app/src/Components/pages/Ads/CreateNewAd/StepTwo.jsx
+++ home-app/src/Components/pages/Ads/CreateNewAd/StepTwo.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-
+import Select from 'react-select'
 /**
  * After user choose rent or buy, move next to fill the ads detailes
  * @returns 
  */
-const StepTwo = () => {
+const StepTwo = ({ formData, setFormData }) => {
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((values) => ({ ...values, [name]: value }));
+  };
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
   return (
-    <div className='stepTwo'>
-      <input type="text" placeholder="Type" className="rounded-input" />
-      <input type="text" placeholder="Type" className="rounded-input" />
-      <input type="text" placeholder="Type" className="rounded-input" />
+    <div className="stepTwo">
+      <Select options={options} onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="עיר"
+        className="rounded-input"
+        name="city"
+        value={formData.city || ""}
+        onChange={handleChange}
+      />
     </div>
   );
-}
+};
 
 export default StepTwo
