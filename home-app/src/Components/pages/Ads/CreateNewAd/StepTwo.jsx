a80465e3232c60c522e7b1d8ef43ad4bb2b3db7a
lidorag <lidorag2@gmail.com>
2022-06-04 16:34:27 +0300

    FIX some no

--- home-app/src/Components/pages/Ads/CreateNewAd/StepTwo.jsx
+++ home-app/src/Components/pages/Ads/CreateNewAd/StepTwo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState } from 'react'
 import Select from 'react-select'
 /**
  * After user choose rent or buy, move next to fill the ads detailes
@@ -6,19 +6,32 @@ import Select from 'react-select'
  */
 const StepTwo = ({ formData, setFormData }) => {
   const handleChange = (event) => {
+    console.log(event);
     const name = event.target.name;
     const value = event.target.value;
     setFormData((values) => ({ ...values, [name]: value }));
   };
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
+
+  const handleChangeCity = (event) => {
+      const value = event.value;
+      setFormData((values) => ({ ...values, "city": value }));
+      setSelectedOption(event);
+    };
+  const [selectedOption, setSelectedOption] = useState({value: formData.city, label: formData.city} || null);
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   return (
     <div className="stepTwo">
-      <Select options={options} onChange={handleChange} />
+      <Select
+        name="city"
+        value={selectedOption}
+        onChange={handleChangeCity}
+        options={options}
+      />
       <input
         type="text"
         placeholder="עיר"
