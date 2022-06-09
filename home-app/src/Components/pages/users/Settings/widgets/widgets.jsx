9d2a2b09d55cefe7b0ed807c95531b83dd2d9b2e
lidorag <lidorag2@gmail.com>
2022-06-09 22:41:06 +0300

    Add widgets to settings->Dashbord

---
+++ home-app/src/Components/pages/users/Settings/widgets/widgets.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import "./styles.css";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoPersonOutline } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
+import { MdOutlineMonetizationOn } from "react-icons/md";
+import { MdAccountBalanceWallet } from "react-icons/md";
+
+const Widgets = ({type}) => {
+  let data;
+
+  /**
+   * here we will use switch to change each block data
+   */
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <IoPersonOutline
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <FiShoppingCart
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MdOutlineMonetizationOn
+            className="icon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <MdAccountBalanceWallet
+            className="icon"
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className="widget">
+      <div className="left">
+        <div className="percentage positive">
+          <IoIosArrowUp />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+      <div className="right">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Widgets
