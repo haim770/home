15a013d727bcd57105b29906a6ffe323679bc63b
lidorag <lidorag2@gmail.com>
2022-06-09 23:45:28 +0300

    Adding charts to our settings

--- home-app/src/Components/pages/users/Settings/Settings.jsx
+++ home-app/src/Components/pages/users/Settings/Settings.jsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
+import Chart from "./Graph/Chart/Chart";
+import Featured from "./Graph/Featured/Featured";
 import Sidebar from "./sidebar/Sidebar";
 import "./styles.css";
 import Widgets from "./widgets/widgets";
-
 const Settings = () => {
   return (
     <div className="settings">
+      <Sidebar />
       <div className="settingsContainer">
         <div className="widgets">
-          <Widgets type="user"/>
-          <Widgets type="order"/>
-          <Widgets type="earning"/>
-          <Widgets type="balance"/>
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earning" />
+          <Widgets type="balance" />
         </div>
         Containter
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+        </div>
       </div>
-      <Sidebar />
     </div>
   );
 };
