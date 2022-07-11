414068f835c7d88c03092577755094038812aae3
haim770 <haim17701@gmail.com>
2022-07-12 00:46:06 +0300

    change user staff with filter between rules

--- home-app/src/Components/ChangeUserRule.js
+++ home-app/src/Components/ChangeUserRule.js
@@ -3,6 +3,7 @@ import Button from "./Button";
 import Address from "./Address";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import UserShow from "./UserShow.js";
+import styles from "../styles/changeUserRules.module.css";
 import "../styles/Register.css";
 import Api from "../api/Api";
 import { v4 as uuidv4 } from "uuid";
@@ -14,10 +15,11 @@ function ChangeUserRule(props) {
   const [mail, setMail] = useState(""); //hook for parameter min value
   const [password, setPassword] = useState(""); //hook for parameter style
   const [phone, setPhone] = useState(""); //hook for parameter style
-  const [UserSelected, setUserSelected] = useState({}); //mail selected
+  const [UserSelected, setUserSelected] = useState(false); //mail selected
   const [mailOptions, setMailOptions] = useState([{}]);
   const [mailSelected, setMailSelected] = useState({});
   const [users, setUsers] = useState(""); //user info
+  const [filter, setFilter] = useState("");
 
   const onChangeState = (setStateName, e) => {
     //func that recieves setstate and the event and change value of state to the value of input
@@ -33,14 +35,52 @@ function ChangeUserRule(props) {
     });
     if (result) {
       setUsers(result.data);
-      result.data.forEach((element) => {
-        setMailOptions((mailOptions) => [
-          ...mailOptions,
-          { label: element.mail, value: element },
-        ]);
-      });
+      result.data
+        .filter((element) =>
+          element.rule.includes(
+            filter === "כולם" ? "" : filter === "משתמש" ? "2001" : "5150"
+          )
+        )
+        .forEach((element) => {
+          setMailOptions((mailOptions) => [
+            ...mailOptions,
+            {
+              label:
+                element.mail +
+                "  " +
+                element.first_name +
+                " " +
+                element.last_name,
+              value: element,
+            },
+          ]);
+        });
     }
     console.log(result.data);
+  };
+  const filterRes = (filterVal) => {
+    setMailOptions([{}]);
+    console.log(filterVal);
+    users
+      .filter((element) =>
+        element.rule.includes(
+          filterVal === "כולם" ? "" : filterVal === "משתמש" ? "2001" : "5150"
+        )
+      )
+      .forEach((element) => {
+        setMailOptions((mailOptions) => [
+          ...mailOptions,
+          {
+            label:
+              element.mail +
+              "  " +
+              element.first_name +
+              " " +
+              element.last_name,
+            value: element,
+          },
+        ]);
+      });
   };
   useEffect(() => {
     getAllUsers();
@@ -56,24 +96,34 @@ function ChangeUserRule(props) {
   return (
     <section className="section_form ">
       <h1>שנה הרשאות למשתמש</h1>
-      <label key="mails">
-        <span>מייל</span>
+      <label>
+        <span>בחר תפקיד משתמש</span>
+        <select
+          id="rule"
+          name="rule"
+          value={filter}
+          onChange={(e) => {
+            setFilter(e.target.value);
+            filterRes(e.target.value);
+          }}
+        >
+          <option>כולם</option>
+          <option>מנהל</option>
+          <option>משתמש</option>
+        </select>
+      </label>
+      <label key="mails" className={styles.lableForSelect}>
+        <span>פרטי המשתמש</span>
         <Select
           className=""
           name="mails"
-          value={mailSelected}
+          value={mailSelected || ""}
           options={mailOptions}
           onChange={handleChangeMail}
           aria-label="users"
         />
       </label>
-      {UserSelected != [] && UserSelected !== <option>no user</option> ? (
-        <p>there is a mail</p>
-      ) : (
-        <p>"no mail selected"</p>
-      )}
-      {users != [] ? <p></p> : <p>"no users"</p>}
-      <UserShow user={UserSelected?UserSelected:[]} act="userIsIn" />
+      {UserSelected ? <UserShow user={UserSelected} act="userIsIn" /> : ""}
     </section>
   );
 }
