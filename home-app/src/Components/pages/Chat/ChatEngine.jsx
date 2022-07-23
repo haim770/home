b79bcbb70849389986030c049d25aba1ae2f77e3
lidorag <lidorag2@gmail.com>
2022-07-23 22:12:01 +0300

    build user settings template

--- home-app/src/Components/pages/Chat/ChatEngine.jsx
+++ home-app/src/Components/pages/Chat/ChatEngine.jsx
@@ -1,51 +1,53 @@
 import { styles } from "./styles";
 import "./styles.css";
-import React, { useState,useRef } from "react";
+import React, { useState } from "react";
 import useAuth from "../../../Auth/useAuth";
 import useView from "./ChatUseContext";
 import ChatWindow from "./ChatWindow";
+
 
 const ChatEngine = () => {
   const { auth } = useAuth();
   const { showContacts, closeWindow, chatWindow } = useView();
   const [hovered, setHovered] = useState(false);
 
-    const handleClick = () => {
-      const chatWith = {
-        adBlock: [],
-        username: "",
-        uuid: "",
-        adID: "",
-      };
-      // if some view is open, then close it on next click otherwise open chat contacts
-      if (chatWindow) {
-        closeWindow(chatWith);
-      } else {
-        showContacts(chatWith);
-      }
+  const handleClick = () => {
+    const chatWith = {
+      adBlock: [],
+      username: "",
+      uuid: "",
+      adID: "",
     };
+    // if some view is open, then close it on next click otherwise open chat contacts
+    if (chatWindow) {
+      closeWindow(chatWith);
+    } else {
+      showContacts(chatWith);
+    }
+  };
 
-
-  return (
-    // if we login show chat, else dont show
-    auth.accessToken ? (
-      <div>
-        <div
-          className="transition-5"
-          onClick={handleClick}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-          style={{
-            ...styles.chatWithMeButton,
-            ...{ border: hovered ? "4px solid #f9f0ff" : "1px solid #f9f0ff" },
-          }}
-        ></div>
-        <ChatWindow />
-      </div>
-    ) : (
-      <></>
-    )
-  );
+    return (
+      // if we login show chat, else dont show
+      auth.accessToken ? (
+        <div>
+          <div
+            className="transition-5"
+            onClick={handleClick}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            style={{
+              ...styles.chatWithMeButton,
+              ...{
+                border: hovered ? "4px solid #f9f0ff" : "1px solid #f9f0ff",
+              },
+            }}
+          ></div>
+          <ChatWindow />
+        </div>
+      ) : (
+        <></>
+      )
+    );
 };
 
 export default ChatEngine;
--- home-app/src/Components/pages/users/Settings/Pages/UserSettings.jsx
+++ home-app/src/Components/pages/users/Settings/Pages/UserSettings.jsx
@@ -1,9 +1,345 @@
-import React from 'react'
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import instance from "../../../../../api/AxiosInstance";
+import "./Styles/UserSettings.css";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
 
-const UserSettings = () => {
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PHONE_REGEX = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+    const handleClickShowPassword = () => {
+      setShowPassword((current) => !current);
+    };
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [user]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // if button enabled with JS hack
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+    try {
+      const response = await instance.request({
+        data: {
+          data_type: "Regist",
+          params: { user, pwd },
+        },
+      });
+      //console.log(response);
+      setSuccess(true);
+      //clear state and controlled inputs
+      setUser("");
+      setPwd("");
+      setMatchPwd("");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else {
+        setErrMsg("Registration Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
   return (
-    <div>UserSettings</div>
-  )
-}
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>הגדרות משתמש</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="Icon-inside">
+          <label htmlFor="username">
+            מייל רישום:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validName ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validName || !user ? "hide" : "invalid"}
+            />
+          </label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+            aria-invalid={validName ? "false" : "true"}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+          />
+        </div>
 
-export default UserSettings
+        <div className="Icon-inside">
+          <label htmlFor="username">
+            שם פרטי:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validName ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validName || !user ? "hide" : "invalid"}
+            />
+          </label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+            aria-invalid={validName ? "false" : "true"}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+          />
+        </div>
+        <div className="Icon-inside">
+          <label htmlFor="username">
+            שם משפחה:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validName ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validName || !user ? "hide" : "invalid"}
+            />
+          </label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+            aria-invalid={validName ? "false" : "true"}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+          />
+        </div>
+
+        <div className="Icon-inside">
+          <label htmlFor="username">
+            מספר פלאפון:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validName ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validName || !user ? "hide" : "invalid"}
+            />
+          </label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+            aria-invalid={validName ? "false" : "true"}
+            aria-describedby="uidnote"
+            onFocus={() => setUserFocus(true)}
+            onBlur={() => setUserFocus(false)}
+          />
+        </div>
+
+
+        <div className="Icon-inside">
+          <label htmlFor="password" className="sameLineLable">
+            סיסמה:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validPwd ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validPwd || !pwd ? "hide" : "invalid"}
+            />
+          </label>
+          <div className="iconInput">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+              aria-invalid={validPwd ? "false" : "true"}
+              aria-describedby="pwdnote"
+              onFocus={() => setPwdFocus(true)}
+              onBlur={() => setPwdFocus(false)}
+            />
+            <span>
+              <IconButton
+                className="IconButton"
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </span>
+          </div>
+          <p
+            id="pwdnote"
+            className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+          >
+            <FontAwesomeIcon icon={faInfoCircle} />
+            4 עד 24 תווים.
+            <br />
+            אותיות, מספרים, קו תחתון ומקף מותרים.{" "}
+            <span aria-label="exclamation mark">!</span>{" "}
+            <span aria-label="at symbol">@</span>{" "}
+            <span aria-label="hashtag">#</span>{" "}
+            <span aria-label="dollar sign">$</span>{" "}
+            <span aria-label="percent">%</span>
+          </p>
+        </div>
+        <div className="Icon-inside">
+          <label htmlFor="confirm_pwd" className="sameLineLable">
+            אמת סיסמה:
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validMatch && matchPwd ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validMatch || !matchPwd ? "hide" : "invalid"}
+            />
+          </label>
+          <div className="iconInput">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="confirm_pwd"
+              onChange={(e) => setMatchPwd(e.target.value)}
+              value={matchPwd}
+              required
+              aria-invalid={validMatch ? "false" : "true"}
+              aria-describedby="confirmnote"
+              onFocus={() => setMatchFocus(true)}
+              onBlur={() => setMatchFocus(false)}
+            />
+            <span>
+              <IconButton
+                className="IconButton"
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </span>
+          </div>
+          <p
+            id="confirmnote"
+            className={matchFocus && !validMatch ? "instructions" : "offscreen"}
+          >
+            <FontAwesomeIcon icon={faInfoCircle} />
+            חייב לתאום את הסיסמה הקודמת.
+          </p>
+        </div>
+        <button
+          disabled={!validName || !validPwd || !validMatch ? true : false}
+        >
+          הירשם
+        </button>
+      </form>
+      <p>
+        כבר רשום?
+        <br />
+        <span className="line">
+          <Link to="/">התחבר</Link>
+        </span>
+      </p>
+    </section>
+  );
+};
+
+export default Register;
+
+/*
+ first name
+last name
+phone number
+mail - not to be change
+password
+prompt
+site rule - view only
+remaining ads - add link to purchase new ads
+
+*/
+const UserSettings = () => {
+  return <div></div>;
+};
+
+//export default UserSettings
