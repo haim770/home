fabd7ce76bb7cd2e3005b604658c16812071106f
haim770 <haim17701@gmail.com>
2022-07-27 19:42:45 +0300

    לג

--- api/system/Ads/reports.php
+++ api/system/Ads/reports.php
@@ -26,15 +26,33 @@ function createReportOnAd(){
   global $db;
   global $DATA_OBJ;
   $userId= $user->getUuid();
+  $element_type=$DATA_OBJ->params->element_type;
   $adID=$DATA_OBJ->params->elementId;
   $reportId=uniqid();
   $content=$DATA_OBJ->params->freeText;
   $title=$DATA_OBJ->params->title;
   $reportType=$DATA_OBJ->params->reportType;
-  $query="INSERT INTO user_reports (report_id,element_id,userId,content,title,report_reason) VALUES ('$reportId','$adID','$userId','$content','$title','$reportType') ";
+  $query="INSERT INTO user_reports (id,element_id,userId,content,title,report_reason,element_type) VALUES ('$reportId','$adID','$userId','$content','$title','$reportType','$element_type') ";
   $result=$db->readDBNoStoredProcedure($query);
   echo json_encode($result);
   die;
+}
+function getAllReports(){
+  //get all reports
+  global $user;
+  if($user->getRule()!="5150"){
+    //not manager
+    echo "not authorized";
+    die;
+  }
+  else{
+    global $db;
+    global $DATA_OBJ;
+    $query="select * from user_reports";
+    $result=$db->readDBNoStoredProcedure($query);
+    echo json_encode($result);
+    die;
+  }
 }
 if($DATA_OBJ->data_type=="reportOnElement"){
 createReportOnAd();
@@ -42,6 +60,11 @@ createReportOnAd();
 else{
   if($DATA_OBJ->data_type=="getAllReportReasons"){
     getAllReportReasons();
+  }
+  else{
+    if($DATA_OBJ->data_type=="getAllReports"){
+      getAllReports();
+    }
   }
 }
 ?>
