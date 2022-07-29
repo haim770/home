9569f982df7d598a74f0937bbb52cd1db2d1cb6a
haim770 <haim17701@gmail.com>
2022-07-29 18:55:59 +0300

    got the chat button of when same user is connected and own ad

--- home-app/src/Components/pages/AdsBlock.jsx
+++ home-app/src/Components/pages/AdsBlock.jsx
@@ -15,6 +15,7 @@ import RecipeReviewCard from "../RecipeReviewCard.js";
 import instance from "../../api/AxiosInstance.jsx";
 import Parameter from "../Parameter.js";
 import useAuth from "../../Auth/useAuth";
+import { gridColumnsTotalWidthSelector } from "@mui/x-data-grid";
 
 const AdsBlock = (props) => {
   /**
@@ -91,28 +92,6 @@ const AdsBlock = (props) => {
               />
             </h3>
           </div>
-          {/* This div will contain add to favorite */}
-          <div className="jss191">
-            <div className="MuiButtonBase-root MuiFab-root jss2248">
-              <span className="MuiFab-label">
-                <svg
-                  version="1.1"
-                  className="jss2250 jss2251"
-                  id="Layer_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 482.207 482.207"
-                >
-                  <path
-                    d="M482.207,186.973l-159.699-33.705L241.104,11.803l-81.404,141.465L0,186.973l109.388,121.134L92.094,470.404l149.01-66.6
-	                  l149.01,66.6l-17.294-162.296L482.207,186.973z M241.104,370.943l-113.654,50.798l13.191-123.788l-83.433-92.393l121.807-25.707
-	                  l62.09-107.9l62.09,107.9L425,205.561l-83.433,92.393l13.191,123.788L241.104,370.943z"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-          </div>
         </header>
 
         {/* This will hold the main assets image */}
@@ -130,7 +109,14 @@ const AdsBlock = (props) => {
         {/** This will contain the Ad footer inner wrapper  */}
         <div className="jss1060">
           {/** This will contain the Ad footer Right button  */}
-          <div className="jss1062">
+          <div
+            className="jss1062"
+            style={{
+              display:
+                props.adBlock.user[0].mail=== auth?.user ? "none" : "block",
+            }}
+          >
+            {console.log(props.adBlock.user[0].mail)}
             <div className="jss142">
               <button
                 className="MuiButtonBase-root MuiButton-root jss151 MuiButton-contained MuiButton-containedPrimary MuiButton-disableElevation MuiButton-fullWidth"
--- home-app/src/Components/pages/AdsBlockForReports.js
+++ home-app/src/Components/pages/AdsBlockForReports.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FaEye } from "react-icons/fa";
 import { FcSms } from "react-icons/fc";
 import { Link, NavLink, Outlet } from "react-router-dom";
-import styles from"../../styles/AdsBlockForReports.module.css";
+import styles from "../../styles/AdsBlockForReports.module.css";
 import "../../styles/Report.css";
 
 // view component for the chat
@@ -104,28 +104,6 @@ const AdsBlockForReports = (props) => {
                 paramValue={props.adBlock.ad.contact_counter}
               />
             </h3>
-          </div>
-          {/* This div will contain add to favorite */}
-          <div className="jss191">
-            <div className="MuiButtonBase-root MuiFab-root jss2248">
-              <span className="MuiFab-label">
-                <svg
-                  version="1.1"
-                  className="jss2250 jss2251"
-                  id="Layer_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 482.207 482.207"
-                >
-                  <path
-                    d="M482.207,186.973l-159.699-33.705L241.104,11.803l-81.404,141.465L0,186.973l109.388,121.134L92.094,470.404l149.01-66.6
-	                  l149.01,66.6l-17.294-162.296L482.207,186.973z M241.104,370.943l-113.654,50.798l13.191-123.788l-83.433-92.393l121.807-25.707
-	                  l62.09-107.9l62.09,107.9L425,205.561l-83.433,92.393l13.191,123.788L241.104,370.943z"
-                  ></path>
-                </svg>
-              </span>
-            </div>
           </div>
         </header>
 
