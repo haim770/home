9df5fd05ac012b6e547f251428e05c88ddd4728c
lidorag <lidorag2@gmail.com>
2022-07-30 19:30:05 +0300

    Messages settings template

--- home-app/src/Components/pages/users/Settings/Pages/Messages.jsx
+++ home-app/src/Components/pages/users/Settings/Pages/Messages.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import instance from "../../../../../api/AxiosInstance";
+import useAuth from "../../../../../Auth/useAuth";
+import { styles } from "./Styles/styles";
+import "./Styles/messagesStyles.css";
 
 const Messages = () => {
-  return (
-    <div>Messages</div>
-  )
-}
+  const { auth } = useAuth();
 
-export default Messages
+  const [hovered, setHovered] = useState(false);
+  const [contacts, setContacts] = useState({});
+
+  /**
+   * Get contacts from server
+   */
+  const getContacts = async () => {
+    const result = await instance.request({
+      data: {
+        data_type: "getContacts",
+        params: {},
+      },
+      headers: {
+        Authorization: `Bearer ${auth.accessToken}`,
+      },
+    });
+    setContacts(result.data.message);
+  };
+
+  function handleClick(firstname, lastname, uuid) {
+    const chatWith = {
+      adBlock: [],
+      username: `${firstname} ${lastname}`,
+      uuid: uuid,
+      adID: "",
+    };
+    // NEED TO ADD START CHAT HERE
+  }
+
+  /**
+   * This use effect will render only once when the component loaded, when we close the contacts it will
+   * end the interval.
+   * Will refesh contacts every 1 second.
+   */
+  useEffect(() => {
+    const Interval = setInterval(() => {
+      getContacts();
+    }, 1000);
+    return () => clearInterval(Interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  /**
+   * This will make that first we get all contacts then we will display all other
+   * data and view, so we will see all contacts when we open the contacts
+   * window
+   */
+  useLayoutEffect(() => {
+    getContacts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  /* This convert object to array and map it */ /*
+  return auth?.accessToken ? (
+    <div
+      className="chatContactsWindow"
+      style={{
+        ...styles.chatBoxWindowContant,
+      }}
+    >
+      {contacts ? (
+        Object.values(contacts).map((contact) => (
+          <div
+            key={contact["uuid"]}
+            className="transition-3 contactWindow"
+            onClick={function () {
+              handleClick(
+                contact["first_name"],
+                contact["last_name"],
+                contact["uuid"]
+              );
+            }}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            style={{
+              ...{
+                backgroundColor: hovered ? "#E0E0E0" : "#ffffff",
+              },
+            }}
+          >
+            {`${contact["first_name"]} ${contact["last_name"]}`}
+          </div>
+        ))
+      ) : (
+        <div>אין צ'טים פעילים</div>
+      )}
+    </div>
+  ) : (
+    <></>
+  );
+};*/
+
+  return (
+    <div className="screenSpliter">
+      <div className="contactsScreen">
+        {auth?.accessToken ? (
+        <div
+          className="chatContactsWindow"
+          style={{
+            ...styles.chatBoxWindowContant,
+          }}
+        >
+          {contacts ? (
+            Object.values(contacts).map((contact) => (
+              <div
+                key={contact["uuid"]}
+                className="transition-3 contactWindow"
+                onClick={function () {
+                  handleClick(
+                    contact["first_name"],
+                    contact["last_name"],
+                    contact["uuid"]
+                  );
+                }}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                style={{
+                  ...{
+                    backgroundColor: hovered ? "#E0E0E0" : "#ffffff",
+                  },
+                }}
+              >
+                {`${contact["first_name"]} ${contact["last_name"]}`}
+              </div>
+            ))
+          ) : (
+            <div>אין צ'טים פעילים</div>
+          )}
+        </div>
+        ) : (<></>
+        )}
+      </div>
+      <div className="messagesScreen">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
---
+++ home-app/src/Components/pages/users/Settings/Pages/Styles/styles.js
@@ -0,0 +1,67 @@
+export const styles = {
+  chatWithMeButton: {
+    cursor: "pointer",
+    boxShadow: "0rem 0rem 1.6rem 0.6rem rgba(0, 0, 0, 0.33)",
+    // Border
+    borderRadius: "50%",
+    // Background
+    backgroundImage: `url(https://i.pravatar.cc/300)`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "8rem",
+    // Size
+    width: "8rem",
+    height: "8rem",
+    zIndex: "1000",
+  },
+  avatarHello: {
+    // Position
+    position: "absolute",
+    left: "calc(-100% - 4.4rem - 2.8rem)",
+    top: "calc(50% - 2.4rem)",
+    // Layering
+    zIndex: "10000",
+    boxShadow: "0rem 0rem 1.6rem 0.6rem rgba(0, 0, 0, 0.33)",
+    // Border
+    padding: "1.2rem 1.2rem 1.2rem 1.6rem",
+    borderRadius: "2.4rem",
+    // Color
+    backgroundColor: "#f9f0ff",
+    color: "black",
+  },
+  chatBoxLogoWrapper: {
+    position: "fixed",
+    display: "flex",
+    flexWrap: "wrap-reverse",
+    flexDirection: "row-reverse",
+    bottom: "3rem",
+    right: "3rem",
+    zIndex: "1000",
+  },
+  chatBoxWindowWrapper: {
+    // Position
+    position: "fixed",
+    bottom: "12.2rem",
+    right: "3.8rem",
+    // Size
+    width: "42.0rem",
+    height: "53.0rem",
+    maxWidth: "calc(100% - 4.8rem)",
+    maxHeight: "calc(100% - 4.8rem)",
+    backgroundColor: "white",
+    // Border
+    borderRadius: "1.2rem",
+    border: `0.2rem solid #f9f0ff`,
+    overflow: "hidden",
+    // Shadow
+    boxShadow: "0rem 0rem 0.4rem 0.2rem rgba(0, 0, 0, 0.33)",
+    zIndex: "1000",
+  },
+  chatBoxWindowContant: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "0.4rem",
+    zIndex: "1000",
+    height: "2rem",
+  },
+};
