20693af785ba3de1ed74f84cef3bf49dc58158fc
lidorag <lidorag2@gmail.com>
2022-08-02 19:51:48 +0300

    fix some errors on my node-module

--- home-app/src/Components/pages/Login.jsx
+++ home-app/src/Components/pages/Login.jsx
@@ -2,10 +2,10 @@ import { useRef, useState, useEffect } from "react";
 import useAuth from "../../Auth/useAuth";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import useInput from "../../Auth/useInput";
-import useToggle from "../../Auth/useInput";
 import instance from "../../api/AxiosInstance";
 import Cookies from "universal-cookie";
 import toast, { Toaster } from "react-hot-toast";
+import "./Styles/loginStyles.css";
 
 const Login = () => {
   const { setAuth } = useAuth();
@@ -19,7 +19,6 @@ const Login = () => {
   const [user, resetUser, userAttribs] = useInput("user", "");
   const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
-  const [check, toggleCheck] = useToggle("persist", true);
 
   const cookies = new Cookies();
 
@@ -49,7 +48,7 @@ const Login = () => {
       setAuth({ user, roles, accessToken });
       // create the refresh token cookie
       cookies.set("refreshToken", refreshTokenCookie, { path: "/" });
-
+      toast.success("התחברת בהצלחה");
       resetUser();
       setPwd("");
       navigate(from, { replace: true });
@@ -68,17 +67,7 @@ const Login = () => {
   };
 
   return (
-    <section className="logRegBox">
-      {location?.state?.act === "registerSucceeds"
-        ? toast.success("ההרשמה הצליחה התחבר בבקשה")
-        : ""}
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
+    <section className="loginContainer">
       <h1>התחברות</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">שם משתמש:</label>
@@ -89,6 +78,7 @@ const Login = () => {
           autoComplete="off"
           {...userAttribs}
           required
+          className="rounded-input"
         />
 
         <label htmlFor="password">סיסמה:</label>
@@ -98,28 +88,77 @@ const Login = () => {
           onChange={(e) => setPwd(e.target.value)}
           value={pwd}
           required
+          className="rounded-input"
         />
         <button>התחבר</button>
-        <div className="persistCheck">
-          <input
-            type="checkbox"
-            id="persist"
-            onChange={toggleCheck}
-            checked={check}
-          />
-          <label htmlFor="persist">זכור אותי</label>
-        </div>
       </form>
-      <p>
-        צריך חשבון?
-        <br />
-        <span className="line">
-          <Link to="/register">הירשם</Link>{" "}
-        </span>
+      <div className="bottom_container">
+        <div>
+          <Link to="/register">שכחת סיסמה? </Link>{" "}
+        </div>
+        <div>
+          <Link to="/register">הירשם </Link>{" "}
+        </div>
+      </div>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
       </p>
-      <Toaster/>
+      <Toaster />
     </section>
   );
 };
 
 export default Login;
+/*
+<section className="logRegBox">
+  <p
+    ref={errRef}
+    className={errMsg ? "errmsg" : "offscreen"}
+    aria-live="assertive"
+  >
+    {errMsg}
+  </p>
+  <h1>התחברות</h1>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="username">שם משתמש:</label>
+    <input
+      type="text"
+      id="username"
+      ref={userRef}
+      autoComplete="off"
+      {...userAttribs}
+      required
+    />
+
+    <label htmlFor="password">סיסמה:</label>
+    <input
+      type="password"
+      id="password"
+      onChange={(e) => setPwd(e.target.value)}
+      value={pwd}
+      required
+    />
+    <button>התחבר</button>
+  </form>
+  <p>
+    צריך חשבון?
+    <br />
+    <span className="line">
+      <Link to="/register">הירשם</Link>{" "}
+    </span>
+  </p>
+
+              <div className="">
+              <GridMui item xs>
+                <Link to="/register">שכחת סיסמה? </Link>{" "}
+              </GridMui>
+              <GridMui item>
+                <Link to="/register">הירשם </Link>{" "}
+              </GridMui>
+            </div>
+  <Toaster />
+</section>;*/
