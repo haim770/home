12878730358978feb7e247ad9ee1629a840823b7
lidorag <lidorag2@gmail.com>
2022-08-11 21:19:45 +0300

    Add roles to settings nav

--- home-app/src/Components/pages/users/Settings/sidebar/Sidebar.jsx
+++ home-app/src/Components/pages/users/Settings/sidebar/Sidebar.jsx
@@ -10,12 +10,14 @@ import { BiPurchaseTagAlt } from "react-icons/bi";
 import { FcSalesPerformance } from "react-icons/fc";
 import { BsClockHistory } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import useAuth from "../../../../../Auth/useAuth";
 const Sidebar = () => {
+   const { auth } = useAuth();
   return (
     <div className="settingsSidebar">
       <div className="settingsCenter">
         <ul>
-          <p className="title">ראשי</p>
+          <p className="titleSet">ראשי</p>
           <li>
             <Link to="/Settings/Dashbord" style={{ textDecoration: "none" }}>
               <RiDashboardLine className="icon" />
@@ -55,7 +57,7 @@ const Sidebar = () => {
               <span>דוחות</span>
             </Link>
           </li>
-          <p className="title">מודעות</p>
+          <p className="titleSet">מודעות</p>
           <li>
             <Link to="/Settings/Ads" style={{ textDecoration: "none" }}>
               <GoHome className="icon" />
@@ -74,74 +76,104 @@ const Sidebar = () => {
               <span>רכישות</span>
             </Link>
           </li>
-          <li>
-            <Link
-              to="/Settings/UserReportsToAds"
-              style={{ textDecoration: "none" }}
-            >
-              <BiPurchaseTagAlt className="icon" />
-              <span>דוחות על מודעות של משתמש</span>
-            </Link>
-          </li>
-          <p className="title">ניהול</p>
-          <li>
-            <Link to="/Settings/Users" style={{ textDecoration: "none" }}>
-              <FiUsers className="icon" />
-              <span>משתמשים</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/Settings/HandlePackages"
-              style={{ textDecoration: "none" }}
-            >
-              <FiUsers className="icon" />
-              <span>נהל חבילות</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/Settings/handleAdParams"
-              style={{ textDecoration: "none" }}
-            >
-              <span> ניהול פרמטרים למודעות</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              to="/Settings/ChangeUserRule"
-              style={{ textDecoration: "none" }}
-            >
-              <span> נהל משתמשים </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/Settings/ManageSiteSettings"
-              style={{ textDecoration: "none" }}
-            >
-              <span> נהל הגדרות אתר </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Settings/Sales" style={{ textDecoration: "none" }}>
-              <FcSalesPerformance className="icon" />
-              <span>מכירות</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/Settings/SitePurchase"
-              style={{ textDecoration: "none" }}
-            >
-              <BsClockHistory className="icon" />
-              <span>היסטורית רכישות</span>
-            </Link>
-          </li>
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/UserReportsToAds"
+                style={{ textDecoration: "none" }}
+              >
+                <BiPurchaseTagAlt className="icon" />
+                <span>דוחות על מודעות של משתמש</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? <p className="titleSet">ניהול</p> : <></>}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link to="/Settings/Users" style={{ textDecoration: "none" }}>
+                <FiUsers className="icon" />
+                <span>משתמשים</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/HandlePackages"
+                style={{ textDecoration: "none" }}
+              >
+                <FiUsers className="icon" />
+                <span>נהל חבילות</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/handleAdParams"
+                style={{ textDecoration: "none" }}
+              >
+                <span> ניהול פרמטרים למודעות</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/ChangeUserRule"
+                style={{ textDecoration: "none" }}
+              >
+                <span> נהל משתמשים </span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/ManageSiteSettings"
+                style={{ textDecoration: "none" }}
+              >
+                <span> נהל הגדרות אתר </span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link to="/Settings/Sales" style={{ textDecoration: "none" }}>
+                <FcSalesPerformance className="icon" />
+                <span>מכירות</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
+          {auth?.roles === "5150" ? (
+            <li>
+              <Link
+                to="/Settings/SitePurchase"
+                style={{ textDecoration: "none" }}
+              >
+                <BsClockHistory className="icon" />
+                <span>היסטורית רכישות</span>
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
         </ul>
       </div>
-      <div className="settingsBottom">Colors</div>
     </div>
   );
 };
