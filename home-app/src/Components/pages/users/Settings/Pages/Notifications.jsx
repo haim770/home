8fcc11eebee3c2af1b6710de9a7a24863d7db6b6
haim770 <haim17701@gmail.com>
2022-08-15 23:19:08 +0300

    notification with link to report page

--- home-app/src/Components/pages/users/Settings/Pages/Notifications.jsx
+++ home-app/src/Components/pages/users/Settings/Pages/Notifications.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect , useState} from "react";
-import "./Styles/Notifications.css"
+import React, { useEffect, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./Styles/Notifications.css";
 import { VscError } from "react-icons/vsc";
 import { IoWarningOutline } from "react-icons/io5";
 import { AiOutlineInfoCircle } from "react-icons/ai";
@@ -9,32 +10,44 @@ import useAuth from "../../../../../Auth/useAuth";
 import NotificationList from "./Notification";
 import EmptyList from "../../../Blog/components/common/EmptyList";
 
-
-
 const Notifications = () => {
   const { auth } = useAuth();
   const [notifications, setNotifications] = useState([]);
-    const getAllNotifications = async () => {
-      const result = await instance.request({
-        data: {
-          data_type: "getAllNotifications",
-          params: {},
-        },
-        headers: {
-          Authorization: `Bearer ${auth.accessToken}`,
-        },
-      });
-      // check if we got new data from server or any response
-      console.log(result?.data);
-      if (result?.data) {
-        setNotifications(result?.data.notifications);
-      }
-    };
+  const getAllNotifications = async () => {
+    const result = await instance.request({
+      data: {
+        data_type: "getAllNotifications",
+        params: {},
+      },
+      headers: {
+        Authorization: `Bearer ${auth.accessToken}`,
+      },
+    });
+    // check if we got new data from server or any response
+    console.log(result?.data);
 
-    useEffect(() => {
-      getAllNotifications();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    if (result?.data) {
+      for (let index = 0; index < result.data.notifications.length; index++) {
+        console.log(result.data.notifications[index]);
+        if (result.data.notifications[index].msgType == "report") {
+          result.data.notifications[index].message_content = (
+            <li>
+              יש לך דיווח חדש על מודעה
+              <Link to={{ pathname: "/settings/UserReportsToAds" }}>
+                לחץ כאן לעמוד הדיווחים
+              </Link>
+            </li>
+          );
+        }
+      }
+      setNotifications(result.data.notifications);
+    }
+  };
+
+  useEffect(() => {
+    getAllNotifications();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="notification-wrapper">
@@ -42,13 +55,13 @@ const Notifications = () => {
       {!notifications.length ? (
         <>
           <EmptyList />
-          <p style={{textAlignLast: "center"}}>נראה שאין התראות</p>
+          <p style={{ textAlignLast: "center" }}>נראה שאין התראות</p>
         </>
       ) : (
         <NotificationList notifications={notifications} />
       )}
     </div>
   );
-}
+};
 
-export default Notifications
+export default Notifications;
