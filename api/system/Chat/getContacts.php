17db9e9b048dff18205075afae7aec12d00ad223
lidorag <lidorag2@gmail.com>
2022-08-27 22:26:10 +0300

    asd

--- api/system/Chat/getContacts.php
+++ api/system/Chat/getContacts.php
@@ -7,8 +7,8 @@ $arr = []; //for global scope var
 $query = "SELECT uuid from users where mail = :alice";
 $arr["alice"] = $token->data->user;
 $arr["alice"] = ($db->readDBNoStoredProcedure($query, $arr))[0]->uuid;
-$query = "SELECT first_name, last_name, uuid from users where uuid in (SELECT DISTINCT sender from messages where receiver =:alice UNION SELECT DISTINCT receiver from messages where sender =:alice)";
-
+//$query = "SELECT first_name, last_name, uuid from users where uuid in (SELECT DISTINCT sender from messages where receiver =:alice UNION SELECT DISTINCT receiver from messages where sender =:alice)";
+$query= "SELECT first_name, last_name, uuid, (SELECT COUNT(messages.seen) FROM `messages` WHERE messages.seen=0 AND messages.receiver=:alice and messages.sender=uuid) as new_prv_msg from users where uuid in (SELECT DISTINCT sender from messages where receiver =:alice UNION SELECT DISTINCT receiver from messages where sender =:alice)";
 $info = (object)[];
 $info->message = $db->readDBNoStoredProcedure($query, $arr);
 $info->data_type = "contactList";
