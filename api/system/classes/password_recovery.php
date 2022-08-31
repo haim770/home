0c00d4b1c7bd8fff14377caf0e8d2f9cff858589
יולו יולחל <haim17701@gmail.com>
2022-08-31 21:52:34 +0000

    password_recovery.php edited online with Bitbucket

--- api/system/classes/password_recovery.php
+++ api/system/classes/password_recovery.php
@@ -7,18 +7,22 @@ class password_recovery
   private string $exp_date;
   protected function getId()
   {
+  //get id
     return $this->id;
   }
   protected function getUserMail()
   {
+  //get userMail
     return $this->userMail;
   }
   protected function getToken()
   {
+  //getToken
     return $this->token;
   }
   protected function getExp_date()
   {
+  //get EXPIRE DATE
     return $this->exp_date;
   }
 }
