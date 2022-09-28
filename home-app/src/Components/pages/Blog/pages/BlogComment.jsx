ec5058eae7e46aeb124d2696a8692464df6c8bfd
lidorag <lidorag2@gmail.com>
2022-09-28 22:18:34 +0300

    Blog comments

--- home-app/src/Components/pages/Blog/pages/BlogComment.jsx
+++ home-app/src/Components/pages/Blog/pages/BlogComment.jsx
@@ -21,7 +21,7 @@ function LinkRecord(props) {
         Authorization: `Bearer ${auth.accessToken}`,
       },
     });
-    
+
     if (result?.data == "not authorized") {
       alert("error");
     } else {
@@ -34,15 +34,40 @@ function LinkRecord(props) {
     const startTime = new Date();
     const endTime = new Date(theTime);
     // To calculate the time difference of two dates
-    var Difference_In_Time = startTime.getTime() - endTime.getTime();
+    // time difference in ms
+    var timeDiff = startTime - endTime;
 
-    // To calculate the no. of days between two dates
-    const numberOfDays = Math.round(Difference_In_Time / (1000 * 3600 * 24));
-    if (numberOfDays>365)
-      return "לפני " + Math.round(numberOfDays / 365) + " שנים";
-    else
-    return "לפני " + numberOfDays + " ימים";
-  }
+    // strip the ms
+    timeDiff /= 1000;
+
+    // get seconds (Original had 'round' which incorrectly counts 0:28, 0:29, 1:30 ... 1:59, 1:0)
+    var seconds = Math.round(timeDiff % 60);
+
+    // remove seconds from the date
+    timeDiff = Math.floor(timeDiff / 60);
+
+    // get minutes
+    var minutes = Math.round(timeDiff % 60);
+
+    // remove minutes from the date
+    timeDiff = Math.floor(timeDiff / 60);
+
+    // get hours
+    var hours = Math.round(timeDiff % 24);
+
+    // remove hours from the date
+    timeDiff = Math.floor(timeDiff / 24);
+
+    // the rest of timeDiff is number of days
+    var days = timeDiff;
+
+    if (days < 1) {
+      if (hours > 1) return "לפני " + hours + " שעות";
+      else if (minutes > 1) return "לפני " + minutes + " דקות";
+      else return "לפני " + seconds + " שניות";
+    } else if (days > 365) return "לפני " + Math.round(days / 365) + " שנים";
+    else return "לפני " + days + " ימים";
+  };
   return (
     <section>
       {showReport === "showCommentEditForm" ? (
@@ -72,9 +97,9 @@ function LinkRecord(props) {
           <li className="commentTitlePart">
             <li className="titleComments">{props.blogComment.title}</li>
           </li>
-          <li>{props.blogComment.content}</li>
+          <li className="userComment">{props.blogComment.content}</li>
           {auth?.roles == "5150" ? (
-            <li>
+            <li className="adminPanel">
               <button className="button-4" onClick={deleteComment}>
                 מחיקת תגובה
               </button>
